c69ecb8e7fa4985b1430b599ef483ab99e8c6131
Siyasanga <siyasanga@live.co.za>
2017-03-07 10:30:35 +0200

    Fix error with the test

--- test/totalPhoneBill.test.js
+++ test/totalPhoneBill.test.js
@@ -3,6 +3,6 @@ describe("totalPhoneBill()",function(){
     assert.equal((totalPhoneBill('call,sms,call,sms,sms')+"").startsWith('7.45'),true);
   });
   it("Return false since the total phone bill is not equal to 7.22",function(){
-    assert.equal((totalPhoneBill('call,sms,call,sms,sms')+"").startsWith('7.45'),false);
+    assert.equal((totalPhoneBill('call,sms,call,sms,sms')+"").startsWith('7.22'),false);
   });
 });
